a07f8a29be0d7131aaa1f5adb46732febb5739c3
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-08-26 00:21:05 +0530

    Logic Done

--- degree-of-seperation/src/App.js
+++ degree-of-seperation/src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Users from "./components/Users";
 import Relationship from "./components/RelationShip";
+import AllUsers from "./components/AllUsers";
 
 function App() {
   const [list, setList] = useState({});
@@ -22,13 +23,58 @@ function App() {
     setList({ ...list });
   }
 
-  console.log(list);
+  let arr = [
+    "Sameer",
+    "Aayushi",
+    "Bhaskar",
+    "Kamalnath Sharma",
+    "Shanti Kumar Saha",
+  ];
+
+  arr.forEach((user) => {
+    addUser(user);
+  });
+
+  useEffect(() => {
+    addFriend("Sameer", "Aayushi");
+    addFriend("Bhaskar", "Aayushi");
+    addFriend("Sameer", "Kamalnath Sharma");
+    addFriend("Kamalnath Sharma", "Shanti Kumar Saha");
+    addFriend("Shanti Kumar Saha", "Bhaskar");
+  }, []);
+
+  useEffect(() => {
+    console.log(list);
+    let results = [];
+    let visited = {};
+    dfs("Sameer", "Bhaskar");
+
+    function dfs(current, end) {
+      console.log(current);
+      visited[current] = true;
+
+      if (current === end) {
+        console.log("Found", current);
+        visited[current] = false;
+        return;
+      }
+
+      for (let i = 0; i < list[current].length; i++) {
+        if (!visited[list[current][i]]) {
+          dfs(list[current][i], end);
+        }
+      }
+    }
+
+    console.log(results);
+  }, []);
 
   return (
     <div className="App">
       <div>Degree of seperation</div>
       <Users addUser={addUser} />
       <Relationship addFriend={addFriend} />
+      <AllUsers list={list} />
     </div>
   );
 }
